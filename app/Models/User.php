1cab77bfaa5445ff536175d4ba89f5f6499b1a7d
jae <ahmadjaelani8685@gmail.com>
2025-10-01 16:12:46 +0700

    add access panel filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Panel;
 
 class User extends Authenticatable
 {
@@ -41,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+     public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, 'https://ahmad-jaelani-portfolio.my.id') && $this->hasVerifiedEmail();
+    }
 }
