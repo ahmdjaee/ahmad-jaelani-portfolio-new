343d1a2519b99f4ad715488b9b738bbdf26ddd93
ahmadjaelani <ahmadjaelani8685@gmail.com>
2026-06-13 19:55:45 +0700

    feat: disable routes web

--- routes/web.php
+++ routes/web.php
@@ -24,54 +24,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth')->group(function () {
-    Route::redirect('/admin-panel', '/admin-panel/project', 301);
-    Route::get('/admin-panel/project', Index::class)->name('admin');
-    Route::get('/admin-panel/project/create', Create::class)->name('admin.project.create');
-    Route::get('/admin-panel/project/edit', Edit::class)->name('admin.project.edit');
+// Route::middleware('auth')->group(function () {
+//     Route::redirect('/admin-panel', '/admin-panel/project', 301);
+//     Route::get('/admin-panel/project', Index::class)->name('admin');
+//     Route::get('/admin-panel/project/create', Create::class)->name('admin.project.create');
+//     Route::get('/admin-panel/project/edit', Edit::class)->name('admin.project.edit');
 
-    Route::get('/admin-panel/blogs', BlogIndex::class)->name('admin.blogs');
-    Route::get('/admin-panel/blogs/create', BlogCreate::class)->name('admin.blogs.create');
-    Route::get('/admin-panel/blogs/edit', BlogEdit::class)->name('admin.blogs.edit');
-});
+//     Route::get('/admin-panel/blogs', BlogIndex::class)->name('admin.blogs');
+//     Route::get('/admin-panel/blogs/create', BlogCreate::class)->name('admin.blogs.create');
+//     Route::get('/admin-panel/blogs/edit', BlogEdit::class)->name('admin.blogs.edit');
+// });
 
-Route::get('/admin-panel/login', [LoginController::class, 'index'])->name('login');
-Route::post('/admin-panel/login', [LoginController::class, 'store'])->name('login.store');
-Route::get('/admin-panel/logout', [LoginController::class, 'logout'])->name('logout');
-
-
-
-Route::get('/', fn() => view('pages.index'));
-Route::get('/about', fn() => view('pages.about'));
-// Route::get('/test', fn() => view('pages.test'));
-Route::get('/contact', fn() => view('pages.contact'));
-Route::get('/blog', fn() => view('pages.blog'));
-Route::get('/resume', fn() => view('pages.resume'));
+// Route::get('/admin-panel/login', [LoginController::class, 'index'])->name('login');
+// Route::post('/admin-panel/login', [LoginController::class, 'store'])->name('login.store');
+// Route::get('/admin-panel/logout', [LoginController::class, 'logout'])->name('logout');
 
 
-Route::get('/project', [ProjectController::class, 'index'])->name('project.index');
-Route::get('/project/{id}', [ProjectController::class, 'show'])->name('project.show');
 
-Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
-Route::get('/blog/{slug}', [BlogController::class, 'show'])->name('blog.show');
+// Route::get('/', fn() => view('pages.index'));
+// Route::get('/about', fn() => view('pages.about'));
+// // Route::get('/test', fn() => view('pages.test'));
+// Route::get('/contact', fn() => view('pages.contact'));
+// Route::get('/blog', fn() => view('pages.blog'));
+// Route::get('/resume', fn() => view('pages.resume'));
 
 
-Route::post('/send-email', [ContactController::class, 'sendEmail']);
+// Route::get('/project', [ProjectController::class, 'index'])->name('project.index');
+// Route::get('/project/{id}', [ProjectController::class, 'show'])->name('project.show');
 
-Route::get('/framework-optimize', function () {
-    Artisan::call('optimize', );
-    Artisan::call('icons:cache', );
+// Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
+// Route::get('/blog/{slug}', [BlogController::class, 'show'])->name('blog.show');
 
-    info('Optimize done!');
 
-    return redirect(route('project.index'));
-});
+// Route::post('/send-email', [ContactController::class, 'sendEmail']);
 
-Route::get('/framework-optimize-clear', function () {
-    Artisan::call('optimize:clear', );
-    Artisan::call('filament:optimize-clear', );
+// Route::get('/framework-optimize', function () {
+//     Artisan::call('optimize', );
+//     Artisan::call('icons:cache', );
 
-    info('Optimize clear done!');
+//     info('Optimize done!');
 
-    return redirect(route('project.index'));
-});
+//     return redirect(route('project.index'));
+// });
+
+// Route::get('/framework-optimize-clear', function () {
+//     Artisan::call('optimize:clear', );
+//     Artisan::call('filament:optimize-clear', );
+
+//     info('Optimize clear done!');
+
+//     return redirect(route('project.index'));
+// });
